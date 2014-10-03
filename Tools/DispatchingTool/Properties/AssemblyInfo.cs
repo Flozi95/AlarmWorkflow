0a1146661662da802f95cb1fceccf59614d9082f
Florian <florian.olching@gmail.com>
2014-10-03 11:52:47 +0200

    Other icon for the DispatchingTool  - svg version for later changes  - ico version for project  - entry in readme (Licenseinformation are missing ... )

--- Tools/DispatchingTool/Properties/AssemblyInfo.cs
+++ Tools/DispatchingTool/Properties/AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using AlarmWorkflow.Shared.Core;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -25,9 +26,9 @@ using System.Windows;
 [assembly: AssemblyTitle("DispatchingTool")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The AlarmWorkflow-Team")]
 [assembly: AssemblyProduct("DispatchingTool")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -66,5 +67,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.8.0")]
+[assembly: AssemblyFileVersion("0.9.8.0")]
+
+[assembly: AlarmWorkflowPackage()]
